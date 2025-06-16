860f19180a279a2b893590b38dbb2c081df0c158
Darknessmanultra <ivan.dorador@alumnos.ucn.cl>
2025-06-16 00:03:38 -0400

    Feat: empty

--- Interacciones/src/Controller/LikesCommentsController.cs
+++ Interacciones/src/Controller/LikesCommentsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Interacciones.src.Models;
+using Interacciones.src.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Interacciones.src.Controller
@@ -10,6 +12,22 @@ namespace Interacciones.src.Controller
     [Route("api/[controller]")]
     public class LikesCommentsController : ControllerBase
     {
-        
+        private readonly LikesRepository _repo;
+
+        public LikesCommentsController(LikesRepository repo) => _repo = repo;
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LikesComments>> Get(int id)
+        {
+            var item = await _repo.GetByIdAsync(id);
+            return item is null ? NotFound() : Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(LikesComments item)
+        {
+            await _repo.CreateAsync(item);
+            return CreatedAtAction(nameof(Get), new { id = item.VideoId }, item);
+        }
     }
 }
---
+++ Interacciones/src/Services/LikesGrpcService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interacciones.src.Services
+{
+    public class LikesGrpcService
+    {
+        
+    }
+}
